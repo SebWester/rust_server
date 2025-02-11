3558aa06ba6f4fe14e1895b9dd599b1e9f5c9c71
Sebastian Westerholm <westerholm.sebastian@gmail.com>
2025-02-11 13:16:18 +0100

    Server up and running

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+// Axum --> HTTP traffic
+// Tokio --> Async runtime 
+// Tower-http --> Serve static files
+use axum::{
+    routing::get,
+    Router,
+};
+use tower_http::services::ServeDir;
+use std::net::SocketAddr;
+
+// Creating a simple server
+#[tokio::main]
+async fn main() {
+    // Create a router
+    let app = Router::new()
+        .route("/", get(|| async { "Welcome to my rust server!" }))
+        .nest_service("/static", ServeDir::new("static"));        
+
+    // Choose port
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("🚀Server running on http://{}", addr);
+
+    // Start server using axum::serve
+    axum::serve(
+        tokio::net::TcpListener::bind(addr).await.unwrap(),
+        app
+    )
+    .await
+    .unwrap();
 }
