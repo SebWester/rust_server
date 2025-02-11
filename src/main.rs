a3fcb25fce64134569d0233293941773242c356d
Sebastian Westerholm <westerholm.sebastian@gmail.com>
2025-02-11 21:11:11 +0100

    Made some changes, trying out get routes

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,30 @@
 use axum::{
     routing::get,
     Router,
+    response::Html,
 };
 use tower_http::services::ServeDir;
 use std::net::SocketAddr;
 
+// Function for homepage
+async fn home() -> Html<&'static str> {
+    Html("<h1>Welcome to my Axum/Rust server</h1>")
+}
+
+// Function for about
+async fn about() -> Html<&'static str> {
+    Html("<h1>About:</h1> <p>This is a Rust server built with Axum</p>")
+}
+
+
 // Creating a simple server
 #[tokio::main]
 async fn main() {
-    // Create a router
+    // Create a router and serve files from static folder
     let app: Router = Router::new()        
-        .nest_service("/", ServeDir::new("static"))
+        .route("/", get(home))
+        .route("/about", get(about))
+        .nest_service("/static", ServeDir::new("static"))
         .fallback(get(|| async { "404: Page not found"}));
 
     // Choose port
